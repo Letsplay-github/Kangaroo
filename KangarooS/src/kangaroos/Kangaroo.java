d57f6be3a0487e5d5f84ce25ed9dcc7d5e88097f
Letsplay-github <55503571+Letsplay-github@users.noreply.github.com>
2020-05-28 21:16:42 +0800

    latest for now

---
+++ KangarooS/src/kangaroos/Kangaroo.java
@@ -0,0 +1,84 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kangaroos;
+import java.util.Random;
+/**
+ *
+ * @author hello
+ */
+public class Kangaroo implements Comparable<Kangaroo> {
+    private Character gender;
+    private int pouchcapacity;
+    private int food = 0;
+    private int pointID;
+    private Random r = new Random();
+    
+    public Kangaroo(){
+        this.gender = null;
+    }
+    public Kangaroo(int pointID, Character gender, int pouchcapacity) {
+        this.pointID = pointID;
+        this.pouchcapacity = pouchcapacity;
+        this.gender = gender;
+        
+    }
+    
+    public int getpointID(){
+        return pointID;
+    }
+    
+    public int getFood(){
+        return food;
+    }
+    
+    public void minusfood(int food){
+        this.food-=food;
+    }
+    
+    public void addFood(int food){
+        this.food+=food;
+    }
+    public Character getGender() {
+        return gender;
+    }
+
+    @Override
+    public int compareTo(Kangaroo o) {
+        if(this.getGender().equals(o.getGender()))
+            return 0;//true
+        else return -1;//false
+  
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return "Kangaroo{" + "gender=" + gender + '}';
+    }
+    
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
